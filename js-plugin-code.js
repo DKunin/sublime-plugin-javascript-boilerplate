26b28c1f26133c7e48cb6af9fcd736910002492d
Dmitri Kunin <whitemarten@gmail.com>
2017-04-24 22:26:55 +0300

    master:Pretty

--- js-plugin-code.js
+++ js-plugin-code.js
@@ -3,11 +3,11 @@ var str = '';
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', function (data) {
+process.stdin.on('data', function(data) {
     str += data;
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', function() {
     try {
         config = JSON.parse(process.argv[2]);
     } catch (e) {
@@ -16,4 +16,3 @@ process.stdin.on('end', function () {
     process.chdir(process.argv[3]);
     process.stdout.write('processed data');
 });
-
